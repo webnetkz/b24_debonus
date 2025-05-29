225c479ef09b2498de090467e64cb47a33936ede
webnetkz <dbqqbq@gmail.com>
2025-05-29 17:19:04 +0500

    Update getTasks, deleted pagination

--- getTasks.php
+++ getTasks.php
@@ -1,9 +1,5 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 require_once './config.php';
 
 if (!isset($_GET['k']) || $_GET['k'] !== $key) {
@@ -14,66 +10,66 @@ require_once './app/db.php';
 require_once './functions.php';
 
 
-if (!isset($_GET['page'])) {
-	$lastPage = $db->query('SELECT last_page FROM `settings`');
-	$lastPage = $lastPage->fetch(PDO::FETCH_ASSOC);
-	$page = $lastPage['last_page'];
-} else { $page = $_GET['page']; }
+$lastPage = $db->query('SELECT last_page FROM `settings`');
+$lastPage = $lastPage->fetch(PDO::FETCH_ASSOC);
+$page = $lastPage['last_page'];
+
+$baseUrl = $url;
+
+while (true) {
+	$url = $baseUrl . '/tasks.task.list.json?start=' . $page;
+	$tasks = getTasksB24($url);
+	$tasks = json_decode($tasks);
+
+	if (empty($tasks->result->tasks)) {
+		$query = "UPDATE `settings`
+					SET last_page = :last_page
+					WHERE id = 1";
+
+		$stmt = $db->prepare($query);
+		$stmt->execute([
+			':last_page' => $page
+		]);
+
+		exit();
+	}
 
 
-$url = $url.'/tasks.task.list.json?start='.$page;
-$tasks = getTasksB24($url);
-$tasks = json_decode($tasks);
+	foreach($tasks->result->tasks as $k => $v) {
+		$stmt = $db->query("SELECT `b_id` FROM `tasks` WHERE `b_id` = '" . $v->id . "'");
+		$result = $stmt->fetch();
 
-if (empty($tasks->result->tasks)) {
-	$query = "UPDATE `settings`
-				SET last_page = :last_page
-				WHERE id = 1";
+	  if (!$result && $v->subStatus == '-1') {
+		preg_match('/!(.*?)!/', $v->title, $matches);
 
-	$stmt = $db->prepare($query);
-	$stmt->execute([
-		':last_page' => $page
-	]);
-	
-	exit();
-}
+		if (@$matches[1]) {
+		  $price = $matches[1];
+		} else {
+		  $price = $fixPrice;
+		}
 
-
-foreach($tasks->result->tasks as $k => $v) {
-	$stmt = $db->query("SELECT `b_id` FROM `tasks` WHERE `b_id` = '" . $v->id . "'");
-  	$result = $stmt->fetch();
-
-  if (!$result && $v->subStatus == '-1') {
-    preg_match('/!(.*?)!/', $v->title, $matches);
-
-    if (@$matches[1]) {
-      $price = $matches[1];
-    } else {
-      $price = $fixPrice;
-    }
-
-    $linkTask = $hostB24.'/company/personal/user/'.$taskShowForId.'/tasks/task/view/'.$v->id.'/';
-	$month = (new DateTime($v->deadline))->format('n');
-    $dataOfTask = [];
-  
-    $dataOfTask = [
-      'b_id' => $v->id,
-      'title' => $v->title,
-      'price' => $price,
-      'deadline' => $v->deadline,
-      'responsible' => $v->responsible->name,
-      'creator' => $v->creator->name,
-      'link' => $linkTask,
-	  'month' => $month
-    ];
-	  
-	if (!empty($dataOfTask)) {
-		insertTaskDB($dataOfTask, $db);
+		$linkTask = $hostB24.'/company/personal/user/'.$taskShowForId.'/tasks/task/view/'.$v->id.'/';
+		$month = (new DateTime($v->deadline))->format('n');
 		$dataOfTask = [];
-  	}
-  }
-}
-	
 
-$page += 50;
-echo '<script> location.href = "./getTasks.php?page=' .$page. '&k='.$key.'" </script>';
+		$dataOfTask = [
+		  'b_id' => $v->id,
+		  'title' => $v->title,
+		  'price' => $price,
+		  'deadline' => $v->deadline,
+		  'responsible' => $v->responsible->name,
+		  'creator' => $v->creator->name,
+		  'link' => $linkTask,
+		  'month' => $month
+		];
+
+		if (!empty($dataOfTask)) {
+			insertTaskDB($dataOfTask, $db);
+			$dataOfTask = [];
+		}
+	  }
+	}
+
+
+	$page += 50;
+}
