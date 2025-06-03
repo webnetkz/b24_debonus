0ff10e6e199d8711c6bf1d8645065eb9aeabfca0
webnetkz <dbqqbq@gmail.com>
2025-06-03 14:18:01 +0500

    Change fixPrice == 200, update debonus getTasks

--- getTasks.php
+++ getTasks.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once './config.php';
 
 if (!isset($_GET['k']) || $_GET['k'] !== $key) {
@@ -11,10 +15,6 @@ require_once './functions.php';
 
 $webhookUrl = $hostB24.$hookMsg.'/im.notify';
 
-// $lastPage = $db->query('SELECT last_page FROM `settings`');
-// $lastPage = $lastPage->fetch(PDO::FETCH_ASSOC);
-// $page = $lastPage['last_page'];
-
 $page = 0;
 $baseUrl = $url;
 
@@ -24,55 +24,51 @@ while (true) {
 	$tasks = json_decode($tasks);
 
 	if (empty($tasks->result->tasks)) {
-		// $query = "UPDATE `settings`
-		// 			SET last_page = :last_page
-		// 			WHERE id = 1";
-
-		// $stmt = $db->prepare($query);
-		// $stmt->execute([
-		// 	':last_page' => $page
-		// ]);
-
 		exit('END');
 	}
 
 
 	foreach($tasks->result->tasks as $k => $v) {
-		$stmt = $db->query("SELECT `b_id` FROM `tasks` WHERE `b_id` = '" . $v->id . "'");
+		$stmt = $db->query("SELECT `b_id`, `responsible_id`, `check` FROM `tasks` WHERE `b_id` = '" . $v->id . "'");
 		$result = $stmt->fetch();
-
+		
 	  if (!$result && $v->subStatus == '-1') {
-		preg_match('/!(.*?)!/', $v->title, $matches);
+		  preg_match('/!(.*?)!/', $v->title, $matches);
 
-		if (@$matches[1]) {
-		  $price = $matches[1];
-		} else {
-		  $price = $fixPrice;
-		}
+      if (@$matches[1]) {
+        $price = $matches[1];
+      } else {
+        $price = $fixPrice;
+      }
 
-		$linkTask = $hostB24.'/company/personal/user/'.$taskShowForId.'/tasks/task/view/'.$v->id.'/';
-		$month = (new DateTime($v->deadline))->format('n');
-		$dataOfTask = [];
+      $linkTask = $hostB24.'/company/personal/user/'.$taskShowForId.'/tasks/task/view/'.$v->id.'/';
+      $month = (new DateTime($v->deadline))->format('n');
+      $dataOfTask = [];
 
-		$dataOfTask = [
-		  'b_id' => $v->id,
-		  'title' => $v->title,
-		  'price' => $price,
-		  'deadline' => $v->deadline,
-		  'responsible' => $v->responsible->name,
-		  'creator' => $v->creator->name,
-		  'link' => $linkTask,
-		  'month' => $month
-		];
+      $dataOfTask = [
+        'b_id' => $v->id,
+        'title' => $v->title,
+        'price' => $price,
+        'deadline' => $v->deadline,
+        'responsible' => $v->responsible->name,
+        'creator' => $v->creator->name,
+        'link' => $linkTask,
+        'month' => $month
+      ];
 
-		if (!empty($dataOfTask)) {
-			insertTaskDB($dataOfTask, $db);
-      sendNotifyB24($webhookUrl, $v->responsible->id, 'Вас депремировали, детали в задаче: '.	$linkTask = $hostB24.'/company/personal/user/'.$v->responsible->id.'/tasks/task/view/'.$v->id.'/');
-			$dataOfTask = [];
-		}
-	  }
+      if (!empty($dataOfTask)) {
+        insertTaskDB($dataOfTask, $db);
+        //sendNotifyB24($webhookUrl, $v->responsible->id, 'Вас депремировали, детали в задаче: '.	$linkTask = $hostB24.'/company/personal/user/'.$v->responsible->id.'/tasks/task/view/'.$v->id.'/');
+        $dataOfTask = [];
+      }
+	  } else {
+		  $fixPrice = (int)$fixPrice;
+
+      $SQL = "UPDATE tasks SET price = price + {$fixPrice} WHERE b_id = '{$result['b_id']}'";
+      $db->query($SQL);	  
+      //sendNotifyB24($webhookUrl, $result['responsible_id'], 'Вас повторно депремировали, детали в задаче: '.	$linkTask = $hostB24.'/company/personal/user/'.$result['responsible_id'].'/tasks/task/view/'.$v->id.'/');
+    }
 	}
-
 
 	$page += 50;
 }
